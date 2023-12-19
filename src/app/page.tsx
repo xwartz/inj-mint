d8a285ccbde9e5b735c60b194277bed2251d8a55
xwartz <stddup@gmail.com>
2023-12-19 22:52:01 +0800

    chore: init

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Minter from "@/components/Minter"
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center space-x-48 p-24">
+      <Minter />
+    </main>
+  )
+}
